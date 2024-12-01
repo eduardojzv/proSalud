bad7bea6df364dd545c436f7f807a32b7ec25215
eduardojzv <ezamorava@est.utn.ac.cr>
2024-12-01 01:50:13 -0600

    updates

--- backEnd/db.py
+++ backEnd/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 # Configuración de la base de datos
 dev="mysql+pymysql://root:root123@localhost:3306/ProSalud"
-engine = create_engine(dev)
+production="mysql+pymysql://root:HFLtNtyEBgdTYcQTIDovRdsFgjiieijT@autorack.proxy.rlwy.net:59579/railway"
+engine = create_engine(production)
 # Dependencia para obtener la sesión de la base de datos
 def get_session():
     session = Session(engine)
