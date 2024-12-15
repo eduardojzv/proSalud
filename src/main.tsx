f20afaabac4bf6fdb95338a7e07e6c23fa683163
eduardojzv <ezamorava@est.utn.ac.cr>
2024-12-15 11:18:28 -0600

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import "./translations/i18n"
 import { router } from './routes/router.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from './providers/themeProviders.tsx'
